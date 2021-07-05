f4ba8d5725c3c646dbd96bd123d278d8d4350afc
Ambujaksh-shah <shah.ambuj08@gmail.com>
2021-07-05 11:33:42 +0530

    First

---
+++ build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ambuj/udacity_project/Openstreet_project/thirdparty/googletest/googlemock
+# Build directory: /home/ambuj/udacity_project/Openstreet_project/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ambuj/udacity_project/Openstreet_project/thirdparty/googletest/googletest
+# Build directory: /home/ambuj/udacity_project/Openstreet_project/build/thirdparty/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
